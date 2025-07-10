079d99bae2300107614956be18f508b0a6cb6511
Shrey-Singhal <shreysinghal1411@gmail.com>
2025-07-10 17:23:08 +1000

    indentation set to 4

--- chess-ui/src/App.tsx
+++ chess-ui/src/App.tsx
@@ -1,13 +1,12 @@
-import SetFEN from './components/setFEN'
+import SetFEN from "./components/setFEN";
 
 function App() {
-
-  return (
-    <>
-      <h1 className='font-bold text-red-500'>Chess</h1>
-      <SetFEN />
-    </>  
-  )
+    return (
+        <>
+            <h1 className="font-bold text-red-500">Chess</h1>
+            <SetFEN />
+        </>
+    );
 }
 
-export default App
+export default App;
