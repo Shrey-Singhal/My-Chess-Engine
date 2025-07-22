e15286257304dc32362731b5fb70d86c0dc4c410
Shrey-Singhal <shreysinghal1411@gmail.com>
2025-07-23 00:42:26 +1000

    change middleware order to fix CORS issues

--- ChessEngineAPI/Program.cs
+++ ChessEngineAPI/Program.cs
@@ -7,19 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+
 
 // // register ChessEngineState class with dependency injection.
 // builder.Services.AddSingleton<ChessEngineState>();
-
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromHours(1);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-builder.Services.AddSingleton<ConcurrentDictionary<string, ChessEngineState>>();
 
 
 builder.Services.AddCors(options =>
@@ -38,12 +29,25 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(1);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddSingleton<ConcurrentDictionary<string, ChessEngineState>>();
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // // Force ChessEngineState initialization
 // app.Services.GetRequiredService<ChessEngineState>();
-app.UseSession();
+app.UseHttpsRedirection();
+
 app.UseCors("AllowAll");
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -51,8 +55,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.MapControllers();
 
